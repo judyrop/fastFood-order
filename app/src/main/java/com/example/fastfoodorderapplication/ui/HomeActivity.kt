8847b8b7c07f999288893f07e034ab393be95a7e
judyrop <judyrop97@gmail.com>
2023-02-01 12:11:18 +0300

    connected app with firebase

--- app/src/main/java/com/example/fastfoodorderapplication/ui/HomeActivity.kt
+++ app/src/main/java/com/example/fastfoodorderapplication/ui/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.fastfoodorderapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fastfoodorderapplication.R
+import com.example.fastfoodorderapplication.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/fastfoodorderapplication/ui/LoginActivity.kt
+++ app/src/main/java/com/example/fastfoodorderapplication/ui/LoginActivity.kt
@@ -1,12 +1,56 @@
 package com.example.fastfoodorderapplication.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.fastfoodorderapplication.R
+import com.example.fastfoodorderapplication.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.accountTv.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginB.setOnClickListener {
+            login()
+        }
+//        binding.forgotPTv.setOnClickListener {
+//            startActivity(Intent(this,ForgotPasswordActivity::class.java))
+//        }
+    }
+
+    private fun login() {
+        val email = binding.emailInput.text.toString()
+        val password = binding.passwordInput.text.toString()
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "You are logged in successfully", Toast.LENGTH_SHORT).show()
+                val  intent = Intent(this,HomeActivity::class.java)
+               intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+            else {
+                Toast.makeText(this,task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
